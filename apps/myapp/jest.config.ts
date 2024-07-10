8bde558e0d8be3273f8e8afd7d331c3bfddfdf25
Chan Lee <chan.lee@umbrage.com>
2024-07-10 16:37:35 +0000

    default remix app

---
+++ apps/myapp/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'myapp',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/myapp',
+};
